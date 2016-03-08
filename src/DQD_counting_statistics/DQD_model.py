75ef885e322e44f8e3cc7388c46c0a45ed3c6f01
Richard Stones <r.stones@ucl.ac.uk>
2016-03-08 15:22:43 +0000

    Initial commit

---
+++ src/DQD_counting_statistics/DQD_model.py
@@ -0,0 +1,12 @@
+'''
+Created on 8 Mar 2016
+
+@author: rstones
+'''
+import numpy as np
+from counting_statistics.counting_statistics import CountingStatistics
+
+class DQDModel(CountingStatistics):
+    
+    def __init__(self):
+        pass
