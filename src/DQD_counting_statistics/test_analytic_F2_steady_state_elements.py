8db9b424726fde6283289aa574a470498c2a71a5
rstones <richardstones10@gmail.com>
2016-05-27 15:56:10 +0100

    Updated analytic expression

--- src/DQD_counting_statistics/test_analytic_F2_steady_state_elements.py
+++ src/DQD_counting_statistics/test_analytic_F2_steady_state_elements.py
@@ -6,12 +6,13 @@ Created on 23 May 2016
 import numpy as np
 import matplotlib.pyplot as plt
 import quant_mech.utils as utils
-from DQD_counting_statistics.DQD_model import DQDModel
+#from DQD_counting_statistics.DQD_model import DQDModel
 
-model = DQDModel(remove_elements=True)
+#model = DQDModel(remove_elements=True)
 
 epsilon_values = np.linspace(-10, 10, 200)
 F2_values = np.zeros(epsilon_values.size)
+steady_states = np.zeros((epsilon_values.size, 5))
 
 T_c = 3.
 Gamma_L = 1.
@@ -33,12 +34,20 @@ def F2(Gamma_R, Gamma_L, epsilon, T_c):
     real_rho_LR = steady_state[3]
     imag_rho_LR = steady_state[4]
     return 1. + (2. / (0.25*(Gamma_R**2)*Gamma_L + 2.*(T_c**2)*Gamma_L + 4.*(epsilon**2)*Gamma_L + (T_c**2)*Gamma_R)) \
+                * (-(T_c**2)*Gamma_R*rho_LL - (0.25*(Gamma_R**2)*Gamma_L + (T_c**2)*Gamma_R + 4.*(epsilon**2)*Gamma_L)*rho_RR \
+                 + 4.*T_c*epsilon*Gamma_L*real_rho_LR + T_c*Gamma_R*Gamma_L*imag_rho_LR), steady_state
+    '''
+    return 1. + (2. / (0.25*(Gamma_R**2)*Gamma_L + 2.*(T_c**2)*Gamma_L + 4.*(epsilon**2)*Gamma_L + (T_c**2)*Gamma_R)) \
         * ((4.*(T_c**2)*Gamma_R + 4.*(T_c**2)*Gamma_L)*(rho_RR**2) + (2.*(T_c**2)*Gamma_R + 4.*(T_c**2)*Gamma_L)*rho_RR*rho_LL + 4.*(T_c**2)*Gamma_R*rho_RR*rho_00 \
            - (0.25*(Gamma_R**2)*Gamma_L + 4.*(epsilon**2)*Gamma_L + 5.*(T_c**2)*Gamma_R + 4.*(T_c**2)*Gamma_L)*rho_RR \
            - (T_c**2)*Gamma_R*rho_LL + 4.*T_c*epsilon*Gamma_L*real_rho_LR + T_c*Gamma_R*Gamma_L*imag_rho_LR)
+    '''
         
 for i,E in enumerate(epsilon_values):
-    F2_values[i] = F2(Gamma_R, Gamma_L, E, T_c)
-    
+    F2_values[i], steady_states[i] = F2(Gamma_R, Gamma_L, E, T_c)
+
+plt.subplot(121)
 plt.plot(epsilon_values, F2_values)
+plt.subplot(122)
+plt.plot(epsilon_values, steady_states.T[4])
 plt.show()
