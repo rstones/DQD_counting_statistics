0442c2874296ca78a858cb7d51f0b1feafd7ecb9
Richard Stones <r.stones@ucl.ac.uk>
2017-05-16 12:11:40 +0100

    Added multiprocessing to F2 calculation scripts

--- remote_scripts/F2_reorg_energy_calculation.py
+++ remote_scripts/F2_reorg_energy_calculation.py
@@ -4,7 +4,10 @@ import quant_mech.time_utils as tu
 from counting_statistics.sparse.fcs_solver import FCSSolver
 from multiprocessing import Pool
 
-data = np.load('data/F2_reorg_energy_heom_data_vals_1-2.npz')
+re_min = 1
+re_max = 4
+
+data = np.load('data/F2_reorg_energy_heom_data_K9_vals_'+str(re_min)+'-'+str(re_max)+'.npz')
 reorg_energy_values = data['reorg_energy_values']
 # elements = data['elements']
 # indices = data['indices']
@@ -23,7 +26,7 @@ results = np.zeros((reorg_energy_values.size, 3))
 
 def calculate_FCS(reorg_energy):
     print str(reorg_energy) + ' at ' + str(tu.getTime())
-    data_pt = np.load('data/F2_reorg_energy_heom_data_vals_1-2_'+str(reorg_energy)+'.npz')
+    data_pt = np.load('data/F2_reorg_energy_heom_data_K9_vals_'+str(re_min)+'-'+str(re_max)+'_'+str(reorg_energy)+'.npz')
     elements = data_pt['elements']
     indices = data_pt['indices']
     indptrs = data_pt['indptrs']
@@ -35,8 +38,8 @@ def calculate_FCS(reorg_energy):
     return [mean_heom, F2_heom, 0]
     
 if __name__ == '__main__':
-    pool = Pool(2)
+    pool = Pool(3)
     results = np.array(pool.map(calculate_FCS, reorg_energy_values))
         
-np.savez('data/F2_reorg_energy_data.npz', reorg_energy_values=reorg_energy_values, mean_heom=results.T[0], \
+np.savez('data/F2_reorg_energy_data_K9_vals_'+str(re_min)+'-'+str(re_max)+'.npz', reorg_energy_values=reorg_energy_values, mean_heom=results.T[0], \
                 F2_heom=results.T[1], coh_heom=results.T[2])
--- remote_scripts/generate_F2_reorg_energy_data.py
+++ remote_scripts/generate_F2_reorg_energy_data.py
@@ -10,7 +10,7 @@ bias = 2.
 T_c = 1. 
 beta = 0.4
 cutoff = 50.
-K = 10
+K = 9
 N = 6
 
 def environment(reorg_energy, beta, K):
@@ -19,8 +19,8 @@ def environment(reorg_energy, beta, K):
             (OBOscillator(reorg_energy, cutoff, beta, K=K),)]
 
 re_min = 1
-re_max = 2
-reorg_energy_values = np.logspace(re_min, re_max, 3)
+re_max = 4
+reorg_energy_values = np.logspace(re_min, re_max, 120)
 indices = []
 elements = []
 indptrs = []
@@ -31,11 +31,11 @@ def generate_heom(reorg_energy):
     model_heom = DQDHEOMModelSparse(Gamma_L, Gamma_R, bias, T_c, beta=beta, environment=environment(reorg_energy, beta, K), \
                                     K=K, tc=True, trunc_level=N)
     heom_matrix = model_heom.heom_matrix()
-    np.savez('data/F2_reorg_energy_heom_data_vals_'+str(re_min)+'-'+str(re_max)+'_'+str(reorg_energy)+'.npz', reorg_energy=reorg_energy, \
+    np.savez('data/F2_reorg_energy_heom_data_K'+str(K)+'_vals_'+str(re_min)+'-'+str(re_max)+'_'+str(reorg_energy)+'.npz', reorg_energy=reorg_energy, \
                         indices=heom_matrix.indices, elements=heom_matrix.data, indptrs=heom_matrix.indptr)
      
 if __name__ == '__main__':
-    pool = Pool(2)
+    pool = Pool(4)
     pool.map(generate_heom, reorg_energy_values)
 
 '''
@@ -50,6 +50,6 @@ jump_indptrs = jump_matrix.indptr
 dv_pops = model_heom.dv_pops
 shape = jump_matrix.shape
 
-np.savez('data/F2_reorg_energy_heom_data_vals_'+str(re_min)+'-'+str(re_max)+'.npz', reorg_energy_values=reorg_energy_values, \
+np.savez('data/F2_reorg_energy_heom_data_K'+str(K)+'_vals_'+str(re_min)+'-'+str(re_max)+'.npz', reorg_energy_values=reorg_energy_values, \
          shape=shape, jump_elements=jump_elements, jump_indices=jump_indices, jump_indptrs=jump_indptrs, dv_pops=dv_pops)
     
